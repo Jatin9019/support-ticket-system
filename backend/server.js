1887dd7f0ef8c987a4ce0405a5a490523c365b1e
Jatin9019 <jatinchopra3@gmail.com>
2023-09-17 15:55:27 -0400

    backend completed

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Server running using nodemon");
+const exp = require('constants');
+const colors = require('colors');
+
+const express = require('express');
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT;
+connectDB();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res)=>{
+    res.status(201).send("Welcoe to the support desk");
+})
+
+app.use('/api/users',require('./routes/userRoutes'));
+app.use(errorHandler);
+
+app.listen(PORT,()=>console.log(`Server started on Port ${PORT}`));
